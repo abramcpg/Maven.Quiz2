83974aed4ce9d183940d1b59117cb331bea9b5d3
Abram Phillips <abramcpg@hotmail.com>
2019-06-10 10:26:22 -0400

    1.3 Passed minus Range test

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,19 +1,31 @@
 package com.zipcodewilmington.assessment2.part1;
+import java.util.Arrays;
 
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        Integer length = array.length;
+        return (length % 2 ==0);
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        Integer length = stop - start;
+        Integer[] answer = new Integer[length];
+        for (int i = 0; start <= stop; i++) {
+            answer[i] = start;
+            start++;
+        }
+
+        return answer;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+        Integer sum = array[0] + array[1];
+        return sum;
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        int length = array.length;
+        Integer product = array[length-2] * array[length-1];
+        return product;
     }
 }
