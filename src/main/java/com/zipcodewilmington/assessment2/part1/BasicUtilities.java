90e607824d3725a0f3e8b08ca2377bb370e36572
Abram Phillips <abramcpg@hotmail.com>
2019-06-10 09:56:30 -0400

    1.2 Passed

--- src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
@@ -2,18 +2,22 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class BasicUtilities {
     public Boolean isGreaterThan5(Integer value) {
-        return null;
+        return (value >= 5);
     }
 
     public Boolean isLessThan7(Integer value) {
-        return null;
+        return (value <= 7);
     }
 
     public Boolean isBetween5And7(Integer valueToEvaluate) {
-        return null;
+        return (valueToEvaluate >= 5 && valueToEvaluate <= 7);
     }
 
     public Boolean startsWith(String string, Character character) {
-        return null;
+        Character letter = string.charAt(0);
+        string = string.toLowerCase();
+        Character letter3 = string.charAt(0);
+
+        return (character == letter|| character == letter3);
     }
 }
