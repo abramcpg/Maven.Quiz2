b1f2a9e69a31abbbc16ac44ecd31cf2076c8e72e
Abram Phillips <abramcpg@hotmail.com>
2019-06-10 10:56:10 -0400

    2.1 Passed

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,8 +1,21 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import com.sun.tools.javac.util.ArrayUtils;
+import java.util.Arrays;
+
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        int length = array1.length +array2.length;
+        Integer[] answer = new Integer[length];
+        for (int i = 0; i < array1.length; i++) {
+            answer[i] = array1[i];
+        }
+        for (int i = (array1.length); i < length; i++) {
+           answer[i] = array2[i - array1.length];
+        }
+
+
+        return answer;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
